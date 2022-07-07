4ee86d654a91df40f7ac43c3cc11ae2bbf5dd7cb
Дмитрий <dimon.pustovskih@mail.ru>
2022-07-07 19:01:34 +0700

    add main file

---
+++ my_vim/main.c
@@ -0,0 +1,21 @@
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <curses.h>
+
+void sig_winch(int signo) {
+  struct winsize size;
+  ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size);
+  resizeterm(size.ws_row, size.ws_col);
+}
+
+int main () {
+  WINDOW * wnd;
+  
+  initscr();
+  signal(SIGWINCH, sig_winch);
+
+  endwin();
+  exit(EXIT_SUCCESS);
+}
